2f1e0dd64a5770d65aa6d95d59a8c7e4c8a0040f
PolkaDott <andrey.gurt@yandex.ru>
2022-05-21 01:41:17 +0500

    Готов дизайн одного поля для валюты

---
+++ Converter/Converter/Effects/PlainEntry.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace Converter.Effects
+{
+    class PlainEntry : RoutingEffect
+    {
+        public PlainEntry() : base("PlainEntryGroup.PlainEntryEffect")
+        {
+        }
+    }
+}
--- Converter/Converter/View/MainPage.xaml.cs
+++ Converter/Converter/View/MainPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Converter.ViewModel;
 using Xamarin.Forms;
 
 namespace Converter
@@ -13,6 +8,7 @@ namespace Converter
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainViewModel();
         }
     }
 }
